274cbc3fbc1b600ad7606a8e7b85ed19279e9e36
Fritz Rehde <fritzrehde.work@gmail.com>
2022-01-26 23:57:06 +0100

    Finished standings option

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Get the latest football scores, fixtures and tables via the command line.
 
 ## Dependencies
 [jq](https://stedolan.github.io/jq/): JSON processor
+[util-linux](https://github.com/util-linux/util-linux): column
--- footy.sh
+++ footy.sh
@@ -4,4 +4,6 @@ API_KEY=$(cat $HOME/.config/footy/api_key.txt)
 
 # standings
 COMP=$1
-curl -sH "X-Auth-Token: $API_KEY" https://api.football-data.org/v2/competitions/$COMP/standings | jq -r '.standings[0].table[].team.name'
+curl -sH "X-Auth-Token: $API_KEY" https://api.football-data.org/v2/competitions/$COMP/standings | \
+	jq -r '.standings[0].table[] | "\(.position),\(.team.name),\(.won),\(.draw),\(.lost),\(.points)"' | \
+	column -ts "," -N Pos,Team,W,D,L,Pts -R Pos,W,D,L,Pts
