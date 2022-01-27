5525507a464428550f243529d27ec408005f92f0
Fritz Rehde <fritzrehde.work@gmail.com>
2022-01-27 12:55:53 +0100

    Added getopts functionality

--- footy.sh
+++ footy.sh
@@ -2,8 +2,24 @@
 
 API_KEY=$(cat $HOME/.config/footy/api_key.txt)
 
-# standings
-COMP=$1
-curl -sH "X-Auth-Token: $API_KEY" https://api.football-data.org/v2/competitions/$COMP/standings | \
-	jq -r '.standings[0].table[] | "\(.position),\(.team.name),\(.won),\(.draw),\(.lost),\(.points)"' | \
-	column -ts "," -N Pos,Team,W,D,L,Pts -R Pos,W,D,L,Pts
+# == PROCESS OPTIONS ==
+usage() { echo "Usage: $0 [-s <league>] [-h]" 1>&2; exit 1; }
+help_() { echo "Help menu" 1>&2; exit 0; }
+
+while getopts ":s:h" opt; do
+	case "${opt}" in
+		s)
+			# standings
+			COMPETITION=${OPTARG}
+			curl -sH "X-Auth-Token: $API_KEY" https://api.football-data.org/v2/competitions/$COMPETITION/standings | \
+				jq -r '.standings[0].table[] | "\(.position),\(.team.name),\(.playedGames),\(.won),\(.draw),\(.lost),\(.points)"' | \
+				column -ts "," -N Pos,Team,P,W,D,L,Pts -R Pos,P,W,D,L,Pts
+			;;
+		h)
+			help_
+			;;
+		*)
+			usage
+			;;
+	esac
+done
