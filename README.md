b782b1487e901b4b1c0b147465b97e6e1fe8a830
Fritz Rehde <fritzrehde.work@gmail.com>
2022-01-27 15:21:33 +0100

    Updated help messages

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # footy
-Get the latest football scores, fixtures and tables via the command line.
+<i>A simple shell script that displays the latest football scores, fixtures and standings</i>
+
+## Installation
+Since footy uses the data provided by [football-data.org](https://www.football-data.org/), an api-key is required.
+You can register for one [here](https://www.football-data.org/client/register) for free.
+
+footy currently only supports Linux (due to some Linux-specific dependencies).
+### Linux
 
 ## Dependencies
 [jq](https://stedolan.github.io/jq/): JSON processor  
-[util-linux](https://github.com/util-linux/util-linux): column
+[util-linux](https://github.com/util-linux/util-linux): for [column](https://man7.org/linux/man-pages/man1/column.1.html)
--- footy.sh
+++ footy.sh
@@ -2,21 +2,61 @@
 
 API_KEY=$(cat $HOME/.config/footy/api_key.txt)
 
-# == PROCESS OPTIONS ==
-usage() { echo "Usage: $0 [-s <league>] [-h]" 1>&2; exit 1; }
-help_() { echo "Help menu" 1>&2; exit 0; }
+# Set script name and full path
+FOOTY_SH=$(basename "$0")
+FOOTY_FULL_SH="$0"
+# export TODO_SH TODO_FULL_SH
 
-while getopts ":s:h" opt; do
+# == HELP MESSAGES ==
+USAGE="$0 [-s <league>] [-h]"
+
+usage()
+{
+	cat <<-EOF >&2
+	Usage: $USAGE
+	Try '$FOOTY_SH -h' for more information.
+	EOF
+	exit 1
+}
+
+shorthelp()
+{
+	cat <<-EOF
+	Usage: $USAGE
+	-s <league>   Prints the standings in specified league.
+	-k <api-key>  Saves the api-key for www.football-data.org in $HOME/.config/footy/api_key.txt
+	-H            Prints names of all supported leagues or teams.
+	-h            Prints this user documentation.
+	EOF
+	exit 0
+}
+
+longhelp()
+{
+	cat <<-EOF
+	Usage: $USAGE
+	long help
+	EOF
+	exit 0
+}
+
+# == PROCESS OPTIONS ==
+while getopts ":s:hH" opt; do
 	case "${opt}" in
+		h)
+			shorthelp
+			;;
+		H)
+			longhelp
+			;;
+		k)
+			;;
 		s)
 			# standings
 			COMPETITION=${OPTARG}
 			curl -sH "X-Auth-Token: $API_KEY" https://api.football-data.org/v2/competitions/$COMPETITION/standings | \
 				jq -r '.standings[0].table[] | "\(.position),\(.team.name),\(.playedGames),\(.won),\(.draw),\(.lost),\(.points)"' | \
 				column -ts "," -N Pos,Team,P,W,D,L,Pts -R Pos,P,W,D,L,Pts
-			;;
-		h)
-			help_
 			;;
 		*)
 			usage
